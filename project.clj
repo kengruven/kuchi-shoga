00cbc7f4bfef740c19e41ecd8cce30e02d8bdd5e
Ken Harris <kengruven@gmail.com>
2015-11-26 15:36:24 -0800

    Make new Leiningen project

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject kuchi-shoga "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "TBD"
+            :url "??"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot kuchi-shoga.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ test/kuchi_shoga/core_test.clj
@@ -0,0 +1,7 @@
+(ns kuchi-shoga.core-test
+  (:require [clojure.test :refer :all]
+            [kuchi-shoga.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
