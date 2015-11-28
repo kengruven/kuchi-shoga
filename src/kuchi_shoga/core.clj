28c4e9775522bb633a0eb29cd52b9f7fc3d9ecd3
Ken Harris <kengruven@gmail.com>
2015-11-28 14:39:47 -0800

    Port rendering to Clojure

--- src/kuchi_shoga/core.clj
+++ src/kuchi_shoga/core.clj
@@ -1,13 +1,50 @@
 (ns kuchi-shoga.core
-  (:use [compojure.core]
+  (:use [clojure.string :as str]
+        [compojure.core]
         [hiccup.core]
         [hiccup.page])
   (:require (compojure
              [route :as route]
              [handler :as handler])))
 
-(defn root [params]
-  (format "hello, world!  %s" (keys params)))
+
+(def KA-LENGTH 6)
+
+(defn render-hit [x y strength]
+  [:circle {:cx x :cy y :r strength}])
+
+(defn render-line [x y hits]
+  [:g
+   [:line {:x1 (- x 20)
+           :y1 y
+           :x2 (+ 10 (* 20 (count hits)))
+           :y2 y}]
+   (for [[i hit] (map-indexed vector hits)]
+     (if (= hit \x)
+       [:path {:d (format "M%d,%d l%d,%d m0,%d l%d,%d"
+                          (+ x (* 20 i) (- KA-LENGTH)) (- y KA-LENGTH)
+                          (* 2 KA-LENGTH) (* 2 KA-LENGTH)
+                          (* -2 KA-LENGTH)
+                          (* -2 KA-LENGTH) (* 2 KA-LENGTH))}]
+       (render-hit (+ x (* 20 i)) y
+                   ({\* 8 \. 3 #" " 0} hit))))])
+
+(defn render-measure [measure]
+  "Given a string like '*.**', make an SVG block."
+  [:svg {:width (* 20 (inc (count measure)))
+         :height 25}
+   [:g {:class "toplevel"} (render-line 15 15 measure)]])
+
+(defn render-piece [lines]
+  [:table
+   (for [line lines]
+     [:tr
+      [:td {:class "label"} (line :label)]
+      [:td (for [measure (str/split (line :hits) #" ")]
+             (render-measure measure))]
+      [:td {:class "repeat"} (if (line :repeat) (format "×%d" (line :repeat)))]
+      [:td {:class "notes"} (line :notes)]])])
+
 
 (defn matsuri [params]
   (html
@@ -19,20 +56,15 @@
      [:link {:href "kuchi-shoga.css" :rel "stylesheet"}]]
     [:body
      [:h2 "Matsuri Taiko"]
-     [:div {:id "matsuri"}]
-     [:script {:type "text/javascript"}
-      "$(function() {
-         renderPiece($('#matsuri'), [
-                {hits:'*--- *--- *-xx x-x-'},
-                {hits:'*-*- --*- *-xx x-x-'},
-                {hits:'--*- --*- *-xx x-x-'},
-                {hits:'*-x- x-*- *-xx x-x-'},
-                {hits:'**xx *-*- *-xx x-x-'}
-         ]);
-       });"]]]))
+     [:div
+      (render-piece [{:hits "*--- *--- *-xx x-x-"}
+                     {:hits "*-*- --*- *-xx x-x-"}
+                     {:hits "--*- --*- *-xx x-x-"}
+                     {:hits "*-x- x-*- *-xx x-x-"}
+                     {:hits "**xx *-*- *-xx x-x-"}])]]]))
+
 
 (defroutes main-routes
-  (GET "/" [] root)
   (GET "/matsuri" [] matsuri)  ;; TEMPORARY -- static path
   (route/files "/" {:root "public"})
   (route/files "/bower_components" {:root "bower_components"})  ;; FIXME: consolidate these...
